89730a29e7f0124809a5d066fd5b78055d491635
drinkgalaxy <drinkgalaxy1226@gmail.com>
2025-07-05 03:09:38 +0900

    Test: 타임존 스케줄러 테스트

--- src/main/java/com/study/studymanagement/domain/attendance/entity/service/AttendanceScheduler.java
+++ src/main/java/com/study/studymanagement/domain/attendance/entity/service/AttendanceScheduler.java
@@ -27,13 +27,13 @@ public class AttendanceScheduler {
 	//@Scheduled(cron = "0 0 0 * * *")
 	@Scheduled(cron = "0 0 3 * * *") // 초 분 시 (수동으로 생성-테스트용)
 	@Transactional
-	public void createTodayAttendanceForAllUsers() {
+	public void createTodayAttendanceForAllUsers() { // 매일 기존 유저들 새로운 데이터 생성
 		List<User> users = userRepository.findAll();
 		LocalDate today = LocalDate.now();
 
 		for (User user : users) {
 			boolean exists = attendanceRepository.existsByUserAndDate(user, today);
-			if (!exists) {
+			if (!exists) { // 존재하지 않으면 새로운 출석 데이터 생성
 				Attendance attendance = Attendance.builder()
 					.user(user)
 					.date(today)
@@ -42,6 +42,12 @@ public class AttendanceScheduler {
 				user.changeStatus(AttendanceStatus.NO_ATTENDED);
 				user.changeStudyStatus(StudyStatus.PAUSED);
 				attendanceRepository.save(attendance);
+			} else { // 존재하면 미리 휴가를 낸 것이므로 오늘의 공부 상태를 휴가로 설정
+				Attendance todayAttendance = attendanceRepository.findByUserAndDate(user, today);
+				User todayUser = todayAttendance.getUser();
+				if (todayAttendance.getAttendanceStatus() == AttendanceStatus.VACATION) {
+					todayUser.changeStatus(AttendanceStatus.VACATION);
+				}
 			}
 		}
 	}
@@ -69,18 +75,19 @@ public class AttendanceScheduler {
 
 	// 23:59:59 에도 출석 전인 유저는 자동으로 결석 상태 처리
 	//@Scheduled(cron = "59 59 23 * * *")  // 매일 23시 59분 59초
-	@Scheduled(cron = "0 3 3 * * *") // 초 분 시 (수동으로 생성-테스트용)
+	@Scheduled(cron = "0 13 3 * * *") // 초 분 시 (수동으로 생성-테스트용)
 	@Transactional
 	public void markAbsentIfNotAttended() {
 		LocalDate today = LocalDate.now();
 
-		// 오늘 날짜의 출석 중, 아직 출석하지 않은 사람들
+		// 오늘 날짜의 출석 중, 아직 출석하지 않은 사람들은 기록에 결석처리, 연속 학습 일수 0으로 초기화
 		List<Attendance> noAttendanceList = attendanceRepository
 			.findAllByDateAndAttendanceStatus(today, AttendanceStatus.NO_ATTENDED);
 
 		for (Attendance attendance : noAttendanceList) {
 			attendance.changeAttendanceStatus(AttendanceStatus.ABSENT);
 			User user = attendance.getUser();
+			user.changeStatus(AttendanceStatus.ABSENT);
 			user.changeConsecutiveStudyDays(false);
 		}
 	}
